4a83294676f546fb1667ea238b0aeedb6752e850
algamawiy <aliyuassalafiygama@gmail.com>
2023-09-12 02:39:18 -0700

    Added: README.md and main.py file

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, Query, status
+from datetime import datetime, timedelta
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class ResponseModel(BaseModel):
+    slack_name: str
+    current_day: str
+    utc_time: str
+    track: str
+    github_file_url: str
+    github_repo_url: str
+    status_code: int
+    
+
+@app.get("/my_info/", response_model=ResponseModel)
+async def get_info(
+    slack_name: str, track: str
+):
+    
+    current_time = datetime.utcnow()
+
+    # Get the current day of the week
+    current_day_of_week = current_time.strftime("%A")
+
+    # Format the current time as a string
+    current_utc_time = current_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    return ResponseModel(
+        slack_name = slack_name, 
+        current_day = current_day_of_week, 
+        utc_time = current_utc_time,
+        track = track,
+        github_file_url = "",
+        github_repo_url = "https://github.com/algamawiy/hngx_backend_stage1_task",
+        status_code = 200
+        )
